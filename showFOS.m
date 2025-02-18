dbe993547cd8cd2ca9c6637bebde8e54490c6a8c
Noah Damery <nopada515@gmail.com>
2025-02-18 15:05:00 -0600

    Updated Test Stand Structure Calcs
    Vectorized math involving the structural integrity of the test stand joint to improve accuracy

--- showFOS.m
+++ showFOS.m
@@ -1,6 +1,14 @@
-function showFOS(name, strength,stress)
+function showFOS(name, strength, stress, varargin)
 %SHOWFOS - Displays the factor of safety of a given setup
 %   name - Display text to explain stress & FOS
+%   strength - Material strength
+%   stress   - Stress on material
+%   unit (optional) - Unit to display (defaults to MPa)
 
-fprintf('%-25s \tStress: %3.1f MPa \tFOS: %3.2f', name, stress, strength/stress);
+unit = 'MPa';
+if nargin > 3
+    unit = varargin{1};
+end
+
+fprintf('%-25s \tStress: %3.1f %s \tFOS: %3.2f', name, stress, unit, strength/stress);
 end
